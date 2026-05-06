df778e651663781d7494a3152b537c98b36e4487
HOSS STORE <storehoss1@gmail.com>
2026-05-06 10:44:57 +0000

    O do netflix:
    https://youtube.com/shorts/rfVydSVztXc

--- src/app/criador/constants.ts
+++ src/app/criador/constants.ts
@@ -30,6 +30,7 @@ export const THEME_OPTIONS = [
     badge: 'Série',
     color: '#e50914',
     image: 'https://picsum.photos/seed/netflix-theme/400/600',
+    videoUrl: 'rfVydSVztXc',
     demoUrl: '/site/funcionando11111111-jfh8'
   },
   { 
