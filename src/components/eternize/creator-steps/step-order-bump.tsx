ceb811f4bacda9f600348a26f7c7e7e865a94747
HOSS STORE <storehoss1@gmail.com>
2026-05-26 20:15:01 +0000

    Ainda esta com o botão do youtube na frente no tik tok

--- src/components/eternize/creator-steps/step-order-bump.tsx
+++ src/components/eternize/creator-steps/step-order-bump.tsx
@@ -231,8 +231,8 @@ export function StepOrderBump({ onBack, onFinish, date, isPackEnabled, onPackTog
                         <div className="absolute inset-0 pointer-events-none overflow-hidden">
                           <iframe
                             className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full scale-[1.3] border-none"
-                            src={`https://www.youtube-nocookie.com/embed/${module.videoUrl}?mute=1&autoplay=1&loop=1&playlist=${module.videoUrl}&controls=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1&enablejsapi=1&origin=${encodeURIComponent(origin)}&widget_referrer=${encodeURIComponent(origin)}`}
-                            allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            src={`https://www.youtube-nocookie.com/embed/${module.videoUrl}?autoplay=1&mute=1&controls=0&loop=1&playlist=${module.videoUrl}&rel=0&modestbranding=1&playsinline=1&enablejsapi=1&origin=${encodeURIComponent(origin)}`}
+                            allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                             title={module.title}
                           />
                         </div>
--- src/components/eternize/landing/theme-carousel.tsx
+++ src/components/eternize/landing/theme-carousel.tsx
@@ -177,8 +177,8 @@ export function ThemeCarousel() {
                       <div className="absolute inset-0 pointer-events-none overflow-hidden">
                         <iframe
                           className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-full h-full scale-[1.25] border-none"
-                          src={`https://www.youtube-nocookie.com/embed/${videoId}?mute=1&autoplay=1&loop=1&playlist=${videoId}&controls=0&rel=0&modestbranding=1&iv_load_policy=3&disablekb=1&playsinline=1&enablejsapi=1&origin=${encodeURIComponent(origin)}&widget_referrer=${encodeURIComponent(origin)}`}
-                          allow="autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                          src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&mute=1&controls=0&loop=1&playlist=${videoId}&rel=0&modestbranding=1&playsinline=1&enablejsapi=1&origin=${encodeURIComponent(origin)}`}
+                          allow="autoplay; accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                           title={theme.name}
                         />
                       </div>
